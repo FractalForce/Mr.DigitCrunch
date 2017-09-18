696205e4f51b3abf9feb5ffc8f405a1a49aca191
Johnny <mr.jmperkins@gmail.com>
2017-09-17 22:08:04 -0700

    add expression history

--- calc.js
+++ calc.js
@@ -1,16 +1,54 @@
+let history = {
+  entries: [],
+  currentIndex: 0,
+  current: "",
+  mostRecent: "",
+  add: function(expression){
+    if(expression != this.mostRecent) { //No immediate duplicate entries
+      this.entries.push(expression)
+      this.mostRecent = expression;
+    }
+    this.currentIndex = this.entries.length
+  },
+  selectOlder: function(){
+    if(this.currentIndex > 0)
+      this.currentIndex--
+
+    return this.entries[this.currentIndex]
+  },
+  selectNewer: function(){
+    if(this.currentIndex < this.entries.length -1)
+      this.currentIndex++
+
+    return this.entries[this.currentIndex]
+  },  
+}
+
 $(function(){
-  $("#input").focus();
+  $("#input").focus()
   $("#input").focusout(function(){
-    $("#input").focus();
-  });
+    $("#input").focus()
+  })
 
   // $("#doCalc").click(function() {
   //   crunch()    
   // })
   
-  $(document).keypress(function(e) {
-    if(e.which == 13) {
-      crunch()      
+  $(document).keydown(function(e) {    
+    switch(e.which) {
+      case 13: // enter
+        crunch()
+      break
+
+      case 38: // up
+        $("#input").val(history.selectOlder())
+      break
+
+      case 40: // down
+        $("#input").val(history.selectNewer())
+      break
+
+      default: return // exit this handler for other keys
     }
   })
 })
@@ -36,5 +74,8 @@ function crunch() {
   if(result) {
     displayResult(result)
     $("#input").val("")
+    history.add(expression)
+    var d = $('#view')
+    d.scrollTop(d.prop("scrollHeight"))
   }
 }
