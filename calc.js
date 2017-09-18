6afe5b5e3fb9ffc1fc62ee91821223471c59b8ea
Johnny <mr.jmperkins@gmail.com>
2017-09-18 16:10:16 -0700

    add syntax highlighting

--- calc.js
+++ calc.js
@@ -6,7 +6,7 @@ let history = {
   add: function(expression){
     if(expression != this.mostRecent) { //No immediate duplicate entries
       this.entries.push(expression)
-      this.mostRecent = expression;
+      this.mostRecent = expression
     }
     this.currentIndex = this.entries.length
   },
@@ -14,68 +14,140 @@ let history = {
     if(this.currentIndex > 0)
       this.currentIndex--
 
-    return this.entries[this.currentIndex]
+    return this.entries.length ? this.entries[this.currentIndex] : ""
   },
   selectNewer: function(){
-    if(this.currentIndex < this.entries.length -1)
+    if(this.currentIndex < this.entries.length - 1)
       this.currentIndex++
+    else if(this.currentIndex === this.entries.length)
+      return ""
 
-    return this.entries[this.currentIndex]
-  },  
+    return this.entries.length ? this.entries[this.currentIndex] : ""
+  }
 }
 
+let editor,
+    Range
+
 $(function(){
-  $("#input").focus()
-  $("#input").focusout(function(){
-    $("#input").focus()
+  Range = ace.require("ace/range").Range
+
+  view = ace.edit("view")
+
+  view.setOptions({
+    mode: "ace/mode/c_cpp",
+    theme: "ace/theme/chaos",
+    readOnly: true,
+    showGutter: false,
+    animatedScroll: true,
+    autoScrollEditorIntoView: true,
+    highlightGutterLine: false,
+    selectionStyle: "text",
+    showPrintMargin: false,
+    wrap: true,
+    maxLines: Infinity
   })
 
-  // $("#doCalc").click(function() {
-  //   crunch()    
-  // })
-  
-  $(document).keydown(function(e) {    
-    switch(e.which) {
-      case 13: // enter
+  editor = ace.edit("input")  
+
+  editor.setOptions({
+    mode: "ace/mode/c_cpp",
+    theme: "ace/theme/chaos",
+    highlightActiveLine: false,
+    showGutter: false,
+    showPrintMargin: false    
+  })
+
+  editor.focus()
+
+  // editor.setTheme("ace/theme/chaos")
+  // editor.session.setMode("ace/mode/c_cpp")
+
+  //Automatically scrolling cursor into view after selection change this will be disabled 
+  //in the next version set editor.$blockScrolling = Infinity to disable this message
+  editor.$blockScrolling = Infinity
+  view.$blockScrolling = Infinity
+
+  editor.commands.addCommand({
+    name: 'doCalc',
+    bindKey: {win: 'enter',  mac: 'enter'},
+    exec: function(editor) {
         crunch()
-      break
+    },
+    readOnly: true // false if this command should not apply in readOnly mode
+  })
 
-      case 38: // up
-        $("#input").val(history.selectOlder())
-      break
+  editor.commands.addCommand({
+    name: 'older',
+    bindKey: {win: 'up',  mac: 'up'},
+    exec: function(editor) {
+      let older = history.selectOlder()
+      if (history.selectOlder()) editor.setValue(older)
+    },
+    readOnly: true
+  })
 
-      case 40: // down
-        $("#input").val(history.selectNewer())
-      break
+  editor.commands.addCommand({
+    name: 'newer',
+    bindKey: {win: 'down',  mac: 'down'},
+    exec: function(editor) {
+      let newer = history.selectNewer()
+      if (history.selectNewer()) editor.setValue(newer)
+    },
+    readOnly: true
+  })  
+  
+  $(document).keyup(function(e) {
+    editor.focus()
+    // switch(e.which) {
+    //   case 13: // enter
+    //     crunch()
+    //   break
 
-      default: return // exit this handler for other keys
-    }
+    //   case 38: // up
+    //     // $("#input").val(history.selectOlder())
+    //     editor.setValue(history.selectOlder())
+    //   break
+
+    //   case 40: // down
+    //     // $("#input").val(history.selectNewer())
+    //     console.log("balls")
+    //     editor.setValue(history.selectNewer())
+    //   break
+
+    //   default: return // exit this handler for other keys
+    // }
   })
 })
 
 function displayResult(result) {
-  let entry = "<div>" + $("#input").val() + "<br>= " + result + "</div>"
-  $("#view").append(entry)
+  let entry = view.getValue().length ? 
+              "\n\n" + editor.getValue() + "\n= " + result : 
+              editor.getValue() + "\n= " + result
+
+  view.insert(entry)
+
+  // var Range = ace.require("ace/range").Range
+  view.session.addMarker(new Range(view.session.getLength() - 1, 0, view.session.getLength() - 1, 1), "result", "fullLine") //ace_active-line
 }
 
 function crunch() {
-  let expression = $("#input").val(),
+  let expression = editor.getValue(),
       result
 
   try {
     result = math.eval(expression)
   } catch (e) {
-    if (e instanceof SyntaxError) {
-      alert(e.message)
-    }
+    // if (e instanceof SyntaxError) {
+    //   alert(e.message)
+    // }
     alert(e.message)
   }
 
   if(result) {
     displayResult(result)
-    $("#input").val("")
+    editor.setValue("")    
     history.add(expression)
-    var d = $('#view')
-    d.scrollTop(d.prop("scrollHeight"))
+    // $('#view').scrollTop(d.prop("scrollHeight"))
   }
 }
